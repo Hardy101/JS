2be504c4e668825ec9dc104491c3e23defd3a021
Eghosa Ordia <eghordia130@gmail.com>
2024-11-24 17:22:58 +0100

    react components

---
+++ 03_React Intro/introapp/src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul>
+      <li>Fish Mooney</li>
+      <li>Carmine Falcone</li>
+      <li>Carlile Maroni</li>
+      <li>Oswald Cobblepot</li>
+      <li>Jim Gordon</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
